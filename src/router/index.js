230ba618bdc19593fcdd5378511ccd276beb81f0
RicardoRonan <ricardoronanm@gmail.com>
2025-07-04 23:02:35 +0200

    chore: Migrate from Vue CLI to Vite and clean up project structure
    - Removed babel.config.js and vue.config.cjs as they are no longer needed.
    - Updated package.json to replace Vue CLI scripts with Vite commands.
    - Deleted public/index.html to streamline the project structure.

--- src/router/index.js
+++ src/router/index.js
@@ -49,7 +49,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   scrollBehavior(to, from, savedPosition) {
     // always scroll to top
